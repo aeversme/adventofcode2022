d49ac818093cc9601bc5179d8bc5b4d9147dcf54
Alex <aeversme@gmail.com>
2022-12-01 13:36:39 -0700

    add part one solution

--- dec01.py
+++ dec01.py
@@ -3,11 +3,34 @@
 
 import input_handler as ih
 
-with open('calories_sample.txt') as cal:
+with open('calories.txt') as cal:
     calories_raw = cal.readlines()
 
 calories_list = ih.convert_input(calories_raw)
 
-print(calories_raw)
-print(calories_list)
-print(calories_list[3] == '')
+# print(calories_raw)
+# print(calories_list)
+# print(calories_list[3] == '')
+
+
+def sum_calories(calorie_list):
+    new_elf_index = 0
+    calorie_sum = 0
+    elf_calorie_list = []
+    while new_elf_index <= len(calorie_list) + 1:
+        if new_elf_index == len(calorie_list):
+            break
+        for i in range(new_elf_index, len(calorie_list)):
+            if calorie_list[i] != '':
+                new_elf_index += 1
+                calorie_sum += calorie_list[i]
+            else:
+                new_elf_index += 1
+                elf_calorie_list.append(calorie_sum)
+                calorie_sum = 0
+    return elf_calorie_list
+
+
+elf_calories = sum_calories(calories_list)
+# print(elf_calories)
+print(max(elf_calories))
--- input_handler.py
+++ input_handler.py
@@ -1,6 +1,6 @@
 def convert_input(calorie_data):
     calorie_strip = [i.strip('\n') for i in calorie_data]
-    for i in range(len(calorie_strip) - 1):
+    for i in range(len(calorie_strip)):
         if calorie_strip[i] != '':
             calorie_strip[i] = int(calorie_strip[i])
     return calorie_strip
