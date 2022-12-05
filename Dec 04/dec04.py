7666e7dc85a5bb4534d72bef43e826db8277d05b
Alex Eversmeyer <aeversme@gmail.com>
2022-12-04 21:53:36 -0700

    add part one solution

--- Dec 04/dec04.py
+++ Dec 04/dec04.py
@@ -3,8 +3,22 @@
 
 from input_handler import convert_input
 
-with open('elfpairs_sample.txt') as file:
+with open('elfpairs.txt') as file:
     pairs_raw = file.readlines()
 
 pairs = convert_input(pairs_raw)
-print(pairs)
+# print(pairs)
+
+
+def find_subsets(pairs_list):
+    subset_total = 0
+    for set_pair in pairs_list:
+        if set_pair[0].issubset(set_pair[1]) or set_pair[0].issuperset(set_pair[1]):
+            subset_total += 1
+    return subset_total
+
+
+# Part one solution
+
+subsets = find_subsets(pairs)
+print(subsets)
--- Dec 04/input_handler.py
+++ Dec 04/input_handler.py
@@ -1,7 +1,17 @@
+def convert_to_sets(set_list):
+    sets = []
+    for string_set in set_list:
+        string_set_split = string_set.split('-')
+        new_set = set(range(int(string_set_split[0]), int(string_set_split[1]) + 1))
+        sets.append(new_set)
+    return sets
+
+
 def convert_input(data):
     data_strip = [i.strip('\n') for i in data]
     converted_data = []
     for item in data_strip:
         item_split = item.split(',')
-        converted_data.append(item_split)
+        item_set = convert_to_sets(item_split)
+        converted_data.append(item_set)
     return converted_data
