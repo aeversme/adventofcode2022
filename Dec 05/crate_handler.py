0137af7bc1cad0704b2fbc0680f107168d1bb322
Alex Eversmeyer <aeversme@gmail.com>
2022-12-05 09:05:31 -0700

    add part two solution

--- Dec 05/crate_handler.py
+++ Dec 05/crate_handler.py
@@ -1,4 +1,9 @@
 def convert_to_stacks(input_list):
+    """
+    Takes a list of strings with [ and ] characters and returns a 2D list array of 'crates.'
+    :param input_list: list
+    :return: list
+    """
     stacks = []
     stack_count = int(input_list.pop()[-1])
     for _ in range(stack_count):
@@ -29,6 +34,11 @@ def convert_to_stacks(input_list):
 
 
 def convert_procedures(input_list):
+    """
+    Takes a list of strings and returns a 2D list array of 'procedures.'
+    :param input_list: list
+    :return: list
+    """
     procedures_list = []
     for procedure in input_list:
         procedure_split = procedure.split(' ')
@@ -40,21 +50,45 @@ def convert_procedures(input_list):
     return procedures_list
 
 
-def move_crates(crates, procedures):
+def move_crates(crates, procedures, crane9001=False):
+    """
+    Takes a 2D list array of 'crates' and a 2D list array of 'procedures' and an optional Boolean, and returns a 2D
+    list array of moved 'crates.'
+    :param crates: list
+    :param procedures: list
+    :param crane9001:Boolean
+    :return:list
+    """
     for procedure in procedures:
-        # print(f'New procedure: moving {procedure[0]} from stack {procedure[1] - 1} ({len(crates[procedure[1] - 1])}'
-        #       f' crates) to stack {procedure[2] - 1}')
-        for i in range(procedure[0]):
-            # print(f'   Moving crate {crates[procedure[1] - 1][-1]} from {crates[procedure[1] - 1]}')
-            crate_to_move = crates[procedure[1] - 1].pop()
-            crates[procedure[2] - 1].append(crate_to_move)
-            # print(f'      Crate {crate_to_move} moved to {crates[procedure[2] - 1]}')
+        if crane9001:
+            # print(f'Procedure: {procedure}')
+            # print(f'Moving {procedure[0]} crates from {procedure[1]}: {crates[procedure[1] - 1]}')
+            crates_to_move = crates[procedure[1] - 1][-procedure[0]:]
+            # print(f'   Moving crates: {crates_to_move}')
+            for crate in crates_to_move:
+                crates[procedure[2] - 1].append(crate)
+                crates[procedure[1] - 1].pop()
+        else:
+            for i in range(procedure[0]):
+                crate_to_move = crates[procedure[1] - 1].pop()
+                crates[procedure[2] - 1].append(crate_to_move)
+        # print(crates)
     return crates
 
 
-def restack_crates(crate_list, procedure_list):
+def restack_crates(crate_list, procedure_list, part_two=False):
+    """
+    Takes two lists of strings and an optional Boolean, and returns a 2D list array.
+    :param crate_list: list
+    :param procedure_list: list
+    :param part_two: Boolean
+    :return: list
+    """
+    crane9001 = False
+    if part_two:
+        crane9001 = True
     stack_list = convert_to_stacks(crate_list)
     print(stack_list)
     procedure_list = convert_procedures(procedure_list)
-    new_stacks = move_crates(stack_list, procedure_list)
+    new_stacks = move_crates(stack_list, procedure_list, crane9001)
     return new_stacks
--- Dec 05/dec05.py
+++ Dec 05/dec05.py
@@ -8,10 +8,19 @@ with open('crates.txt') as file:
     crates_raw = file.readlines()
 
 crates, procedures = convert_input(crates_raw)
+crates_copy = crates.copy()
 
 
-def find_top_crates(crate_list, procedure_list):
-    moved_crates = ch.restack_crates(crate_list, procedure_list)
+def find_top_crates(crate_list, procedure_list, part_two=False):
+    """
+    Takes a string list of 'crates' and a string list of 'procedures' and an optional Boolean, and returns a string \
+    representing the top 'crate' in each stack.
+    :param crate_list: list
+    :param procedure_list: list
+    :param part_two: Boolean
+    :return: str
+    """
+    moved_crates = ch.restack_crates(crate_list, procedure_list, part_two)
     crates_string = ''
     for stack in moved_crates:
         crates_string += stack[-1]
@@ -22,3 +31,8 @@ def find_top_crates(crate_list, procedure_list):
 
 top_crates = find_top_crates(crates, procedures)
 print(top_crates)
+
+# Part two solution
+
+top_crates_9001 = find_top_crates(crates_copy, procedures, part_two=True)
+print(top_crates_9001)
